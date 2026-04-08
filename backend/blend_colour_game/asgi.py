51620d68df31b6c379cf0e3a0f2f5bc86ae6c749
Abdullahs1214 <abdullah.khan.sherwani@gmail.com>
2026-04-08 10:00:44 -0600

    rudimentary working

--- backend/blend_colour_game/asgi.py
+++ backend/blend_colour_game/asgi.py
@@ -1,17 +1,19 @@
 import os
 
-from django.core.asgi import get_asgi_application
-
-from websockets.room_consumer import websocket_application
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blend_colour_game.settings")
+
+import django
+django.setup()
+
+from django.core.asgi import get_asgi_application
+from websockets.room_consumer import websocket_application
 
 django_application = get_asgi_application()
 
 
 async def application(scope, receive, send):
-    if scope["type"] == "websocket":
-        await websocket_application(scope, receive, send)
-        return
+  if scope["type"] == "websocket":
+    await websocket_application(scope, receive, send)
+    return
 
-    await django_application(scope, receive, send)
+  await django_application(scope, receive, send)
